faef2d2798075545529628c929464700891bf869
Evan099 <891837367@qq.com>
2019-06-18 00:22:24 +0800

    内部类

---
+++ src/com/imooc/hello8/Person.java
@@ -0,0 +1,25 @@
+package com.imooc.hello8;
+
+public class Person {
+	int age;
+	
+	public Heart getHeart() {
+		return new Heart();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	 class Heart{
+		 public String beat() {
+			 return age+"ĞÄÔàÔÚÌø¶¯";
+		 }
+	
+	}
+	
+	
+}
---
+++ src/com/imooc/hello8/Test.java
@@ -0,0 +1,25 @@
+package com.imooc.hello8;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Person lili = new Person();
+		lili.age = 20;
+		
+		
+		//获取内部类对象实例，方式1：new 外部类.new 内部类
+		Person.Heart myHeart = new Person().new Heart();
+		System.out.println(myHeart.beat());
+		
+		//获取内部类对象实例，方式2：外部类对象.new 内部类
+		myHeart = lili.new Heart();
+		System.out.println(myHeart.beat());
+		
+		//获取内部类对象实例，方式3：外部类对象.获取方法
+		myHeart = lili.getHeart();
+		System.out.println(myHeart.beat());
+		
+
+	}
+
+}
